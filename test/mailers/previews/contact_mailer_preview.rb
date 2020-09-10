b042b410223c3e6d26394def91f538baaca440e0
irles24601 <jack_the_fool@outlook.com>
2020-09-10 06:50:36 +0000

    [Add]ContactMail

---
+++ test/mailers/previews/contact_mailer_preview.rb
@@ -0,0 +1,10 @@
+# Preview all emails at http://localhost:3000/rails/mailers/contact_mailer
+class ContactMailerPreview < ActionMailer::Preview
+
+  # Preview this email at http://localhost:3000/rails/mailers/contact_mailer/contact_mail
+  def contact_mail
+    @contact = Contact.new(email: "test@test.com", message: "問い合わせメッセージ")
+    ContactMailer.contact_mail(@contact)
+  end
+
+end
