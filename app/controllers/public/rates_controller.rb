68c1d92650e372f3affe6966718acd1522862a9e
irles24601 <jack_the_fool@outlook.com>
2020-09-18 07:44:20 +0000

    [update]flash ajax

--- app/controllers/public/rates_controller.rb
+++ app/controllers/public/rates_controller.rb
@@ -3,19 +3,19 @@ class Public::RatesController < ApplicationController
     @cocktail = Cocktail.find(params[:cocktail_id])
     rate = Rate.new(rate_params)
     rate.end_user_id = current_end_user.id
-    if rate.save
-      flash.now[:notice] = "コメントを投稿しました。"
-    else
-      flash.now[:alert] = "投稿に失敗しました。"
-      render 'public/cocktail/show'
+    respond_to do |format|
+      rate.save
+      format.js { flash.now[:notice] = 'コメントを投稿しました。' }
     end
   end
 
   def destroy
     @cocktail = Cocktail.find(params[:cocktail_id])
     rate = Rate.find_by(id: params[:id], cocktail_id: params[:cocktail_id])
-    rate.destroy
-    flash.now[:alert] = "コメントを削除しました。"
+    respond_to do |format|
+      rate.destroy
+      format.js { flash.now[:alert] = 'コメントを削除しました。' }
+    end
   end
 
   private
