f4dcd7003dba463e9ef99e16c5638f5e4a6d70ea
irles24601 <jack_the_fool@outlook.com>
2020-10-14 23:44:42 +0000

    [Add]dropdown js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -29,6 +29,17 @@
   });
 })(jQuery);
 
+(function () {
+  document.addEventListener('DOMContentLoaded', function() {
+    const btn = document.getElementById('dropdown__btn');
+    if(btn) {
+      btn.addEventListener('click', function(){
+        this.classList.toggle('is-open');
+      });
+    }
+  });
+}());
+
 // Loading-animation
 jQuery(function() {
 	var loader = $('.loader-wrap');
