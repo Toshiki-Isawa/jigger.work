a1308ecf5d84e784e26d8f1552b24e85e7e0df15
irles24601 <jack_the_fool@outlook.com>
2020-10-15 23:34:07 +0000

    [Add]modal function

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -874,3 +874,15 @@ $(document).ready(function () {
       return false;
   });
 });
+
+// modal
+$(function(){
+  $('.js-modal-open').on('click',function(){
+      $('.js-modal').fadeIn();
+      return false;
+  });
+  $('.js-modal-close').on('click',function(){
+      $('.js-modal').fadeOut();
+      return false;
+  });
+});
