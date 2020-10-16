c019cd1dc72bdc0ff60d52a7c44d36d074a7b5cb
irles24601 <jack_the_fool@outlook.com>
2020-10-16 01:07:29 +0000

    [add]modal content

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -854,27 +854,6 @@ jQuery(function() {
   };
 })(jQuery);
 
-// sidebar
-$(document).ready(function () {
-  // 向き
-  var sides = ["left", "top", "right", "bottom"];
-
-  // サイドバーの初期化
-  for (var i = 0; i < sides.length; ++i) {
-      var cSide = sides[i];
-      $(".sidebar." + cSide).sidebar({side: cSide});
-  }
-
-  // ボタンのクリックで開閉アクション
-  $(".btn[data-action]").on("click", function () {
-      var $this = $(this);
-      var action = $this.attr("data-action");
-      var side = $this.attr("data-side");
-      $(".sidebar." + side).trigger("sidebar:" + action);
-      return false;
-  });
-});
-
 // modal
 $(function(){
   $('.js-modal-open').on('click',function(){
