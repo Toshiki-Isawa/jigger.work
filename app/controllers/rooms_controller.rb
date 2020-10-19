81fc7a9dc25970293877e81f6c56c55d0b941ffd
irles24601 <jack_the_fool@outlook.com>
2020-10-19 05:55:45 +0000

    [update]last_entry_at default

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -3,23 +3,23 @@ class RoomsController < ApplicationController
   def index
     @user = current_end_user
     @currentEntries = current_end_user.entries
-    #@currentEntriesのルームを配列にする
+    # @currentEntriesのルームを配列にする
     myRoomIds = []
     @currentEntries.each do |entry|
       myRoomIds << entry.room.id
     end
-    #@currentEntriesのルーム且つcurrent_userでないEntryを新着順で取ってくる
+    # @currentEntriesのルーム且つcurrent_userでないEntryを新着順で取ってくる
     @anotherEntries = Entry.where(room_id: myRoomIds).where.not(end_user_id: @user.id).order(created_at: :desc)
   end
 
   def show
     @room = Room.find(params[:id])
-    #ルームが作成されているかどうか
+    # ルームが作成されているかどうか
     if Entry.where(:end_user_id => current_end_user.id, :room_id => @room.id).present?
       @direct_messages = @room.direct_messages
       @entries = @room.entries
       # ルームを開いたら入室日時を記録
-      Entry.where(:end_user_id => current_end_user.id, :room_id => @room.id).update(last_entry_at: Time.now)
+      Entry.where(:end_user_id => current_end_user.id, :room_id => @room.id).update(:last_entry_at => Time.now)
     else
       redirect_back(fallback_location: root_path)
     end
@@ -27,10 +27,12 @@ class RoomsController < ApplicationController
 
   def create
     @room = Room.create(:name => "DM")
-    #entryにログインユーザーを作成
+    # entryにログインユーザーを作成
     @entry1 = Entry.create(:room_id => @room.id, :end_user_id => current_end_user.id)
-    #entryにparamsユーザーを作成
+    # entryにparamsユーザーを作成
     @entry2 = Entry.create(params.require(:entry).permit(:end_user_id, :room_id).merge(:room_id => @room.id))
+    # 入室日時の初期値をルームの作成日時で設定
+    @entries = @room.entries.update(:last_entry_at => @room.created_at)
     redirect_to room_path(@room.id)
   end
 
