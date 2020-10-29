10bef06d50c0c752138f2bf959a35d130a86a6c8
irles24601 <jack_the_fool@outlook.com>
2020-10-29 07:24:51 +0000

    [Add]notification room index

--- spec/factories/end_user.rb
+++ spec/factories/end_user.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :end_user do
     name { Faker::Lorem.characters(number:10) }
     email { Faker::Internet.email }
-    introduction { Faker::Lorem.characters(number:20) }
+    # introduction { Faker::Lorem.characters(number:20) }
     birth_date { '1997-01-12' }
     password { 'password' }
     password_confirmation { 'password' }
--- spec/requests/notifications_request_spec.rb
+++ spec/requests/notifications_request_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe "Notifications", type: :request do
+  let(:end_user) { create(:end_user) }
+
+  describe '通知一覧ページ' do
+    context "ログインしていない場合" do
+      it 'HTTP302レスポンスとともにログイン処理にリダイレクト' do
+        get notifications_path
+        expect(response.status).to eq 302
+      end
+    end
+    context "ログインしている場合" do
+      before do
+        sign_in end_user
+      end
+      it 'リクエストは200 OKとなること' do
+        get notifications_path
+        expect(response.status).to eq 200
+      end
+    end
+  end
 
 end
