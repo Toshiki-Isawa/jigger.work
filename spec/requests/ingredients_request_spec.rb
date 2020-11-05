c17b95183e187e141878e17a69182ab46ce5a4ea
irles24601 <jack_the_fool@outlook.com>
2020-11-05 04:45:46 +0000

    [Add]admins ingredient controller spec

--- spec/requests/ingredients_request_spec.rb
+++ spec/requests/ingredients_request_spec.rb
@@ -2,13 +2,15 @@ require 'rails_helper'
 
 RSpec.describe "Ingredients", type: :request do
   let(:ingredient) { create(:ingredient) }
+  let(:admin) { create(:admin) }
 
-  describe '材料一覧ページ' do
-    context "一覧ページが正しく表示される" do
+  describe 'public' do
+    context "index" do
       before do
-        get public_ingredients_path
+        visit public_ingredients_path
       end
       it 'リクエストは200 OKとなること' do
+        get public_ingredients_path
         expect(response.status).to eq 200
       end
       it '材料名が表示される' do
@@ -22,18 +24,57 @@ RSpec.describe "Ingredients", type: :request do
         end
       end
     end
-  end
-
-  describe '検索' do
-    context "検索結果が表示される" do
-      before do
-        get public_ingredients_path
-      end
+    context "search" do
       it '材料検索結果が表示される' do
         Ingredient.type_names.keys.each do |type_name|
           post search_public_ingredients_path(:name => type_name)
           expect(response.status).to eq 200
         end
+      end
+    end
+  end
+
+  describe 'admin' do
+    before do
+      sign_in admin
+    end
+    context "index" do
+      before do
+        visit admins_ingredients_path
+      end
+      it 'リクエストは200 OKとなること' do
+        get public_ingredients_path
+        expect(response.status).to eq 200
+      end
+      it '材料名が表示される' do
+  			expect(page).to have_content ingredient.name
+      end
+      it '材料種別が表示される' do
+        Ingredient.type_names.keys.each do |type_name|
+          expect(page).to have_content type_name
+        end
+      end
+    end
+    context "search" do
+      it '材料検索結果が表示される' do
+        Ingredient.type_names.keys.each do |type_name|
+          post search_admins_ingredients_path(:name => type_name)
+          expect(response.status).to eq 200
+        end
+      end
+    end
+
+    context "create/destroy" do
+      before do
+        visit admins_ingredients_path
+      end
+      it '材料追加できる' do
+        fill_in 'ingredient_name', with: 'test'
+        fill_in 'ingredient_alcohol', with: 40
+        click_button '追加'
+      end
+      it '材料追加に失敗する' do
+        click_button '追加'
       end
     end
   end
